7994ff89f1e2bc1e9079f727a227373041454137
Anfal Mushtaq <muhammadanfal0@gmail.com>
2018-06-24 22:22:03 +0500

    Added normal validation

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
 
-  var normalValidationFormClass = ".normal-validation";
+  var normalValidationFormClass = ".normal-validation",
+      normalFormSubmitButton = ".normal-form-submit-button",
+      normalFormClearButton = ".normal-form-clear-button";
+
+  $(normalFormSubmitButton).click(function(e) {
+    e.preventDefault();
+
+    if($(normalValidationFormClass).leanValidator("validate")) {
+      alert("Normal Form is good.");
+    }
+
+  });
+
+  $(normalFormClearButton).click(function(e) {
+    e.preventDefault();
+
+    $(normalValidationFormClass).leanValidator("clear");
+  });
+
+  $(normalValidationFormClass).leanValidator();
 
 });
